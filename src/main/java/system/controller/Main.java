e9eaef1369be1a6102e54d310948298e1a99484f
Riccardo Pressiani <riccardo.pressiani@gmail.com>
2017-11-28 20:11:11 -0600

    Add generated code folder structure

--- src/main/java/system/controller/Main.java
+++ src/main/java/system/controller/Main.java
@@ -1,4 +1,11 @@
 package system.controller;
 
+import java.io.File;
+
 public class Main {
+
+    public static void main(String[] args) {
+        new File("generatedSrc/main/java").mkdirs();
+    }
+
 }
