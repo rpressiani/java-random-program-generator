c8382399ffc2c30266884e8bc870eacf7eb5846a
Riccardo Pressiani <riccardo.pressiani@gmail.com>
2017-11-29 13:44:44 -0600

    Add remaining integral types

--- src/main/java/system/model/nodes/types/IntegralType.java
+++ src/main/java/system/model/nodes/types/IntegralType.java
@@ -10,10 +10,23 @@ package system.model.nodes.types;
 
 import system.model.nodes.Node;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class IntegralType implements Node {
+
+    List<String> types = new ArrayList<String>(){{
+        add("byte");
+        add("short");
+        add("int");
+        add("long");
+        add("char");
+    }};
+
     @Override
     public String produce() {
 //        TODO implements other types
-        return this.verify("int ");
+        return this.verify(types.get(new Random().nextInt(types.size() - 1)) + " ");
     }
 }
