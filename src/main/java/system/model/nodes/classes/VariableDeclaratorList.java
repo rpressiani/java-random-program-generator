be08b19e8403bb6837a53da53fb2e279753bea02
Riccardo Pressiani <riccardo.pressiani@gmail.com>
2017-12-01 16:52:10 -0600

    Add variableDeclaratorList with more than one var

--- src/main/java/system/model/nodes/classes/VariableDeclaratorList.java
+++ src/main/java/system/model/nodes/classes/VariableDeclaratorList.java
@@ -4,22 +4,39 @@ package system.model.nodes.classes;
 //        :	variableDeclarator (',' variableDeclarator)*
 //        ;
 
+import system.controller.Main;
 import system.model.nodes.Node;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class VariableDeclaratorList implements Node {
 
-    private VariableDeclarator variableDeclarator;
+    private List<VariableDeclarator> variableDeclaratorList;
 
     VariableDeclaratorList() {
-        this.variableDeclarator = new VariableDeclarator();
+        this.variableDeclaratorList = new ArrayList<>();
+        int listLen = Main.config.getVariableDeclaratorListLength();
+
+        for (int i = 0; i < (new Random()).nextInt(listLen) + 1; i++) {
+            this.variableDeclaratorList.add(new VariableDeclarator());
+        }
     }
 
-    public VariableDeclarator getVariableDeclarator() {
-        return variableDeclarator;
+    public List<VariableDeclarator> getVariableDeclaratorList() {
+        return variableDeclaratorList;
     }
 
     @Override
     public String produce() {
-        return this.verify(this.variableDeclarator.produce());
+        StringBuilder b = new StringBuilder();
+        b.append(this.variableDeclaratorList.get(0).produce());
+
+        for (int i = 1; i < this.variableDeclaratorList.size() ; i++) {
+            b.append("," + this.variableDeclaratorList.get(i).produce());
+        }
+
+        return this.verify(b.toString());
     }
 }
--- src/main/java/utils/Config.java
+++ src/main/java/utils/Config.java
@@ -6,13 +6,15 @@ public class Config {
 
     private Map<String, String> identifier;
     private Map<String, Integer> fields;
+    private int variableDeclaratorListLength;
 
     public Map<String, String> getIdentifier() {
         return identifier;
     }
-
     public Map<String, Integer> getFields() {
         return fields;
     }
-
+    public int getVariableDeclaratorListLength() {
+        return variableDeclaratorListLength - 1;
+    }
 }
