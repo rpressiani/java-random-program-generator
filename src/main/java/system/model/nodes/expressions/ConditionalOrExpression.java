a968440af1cc469358ad27edd26c4a325b5f96b7
Riccardo Pressiani <riccardo.pressiani@gmail.com>
2017-12-07 10:40:23 -0600

    Add OR

--- src/main/java/system/model/nodes/expressions/ConditionalOrExpression.java
+++ src/main/java/system/model/nodes/expressions/ConditionalOrExpression.java
@@ -3,6 +3,7 @@ package system.model.nodes.expressions;
 import system.model.STKey;
 import system.model.ScopeTable;
 import system.model.nodes.Node;
+import utils.RandomGen;
 
 //conditionalOrExpression
 //        :	conditionalAndExpression
@@ -13,13 +14,27 @@ import system.model.nodes.Node;
 public class ConditionalOrExpression implements Node {
 
     private ConditionalAndExpression conditionalAndExpression;
+    private ConditionalOrExpression conditionalOrExpression;
+
+    private boolean unary;
 
     ConditionalOrExpression(STKey key, ScopeTable scopeTable) {
+        this.unary = (RandomGen.getNextInt(10) != 0);
+
         this.conditionalAndExpression = new ConditionalAndExpression(key, scopeTable);
+
+        if (!this.unary) {
+            this.conditionalOrExpression = new ConditionalOrExpression(key, scopeTable);
+        }
     }
 
     @Override
     public String produce() {
-        return this.verify(this.conditionalAndExpression.produce());
+        if (this.unary) {
+            return this.verify(this.conditionalAndExpression.produce());
+        } else {
+            String prod = this.conditionalOrExpression.produce() + "||" + this.conditionalAndExpression.produce();
+            return this.verify(prod);
+        }
     }
 }
