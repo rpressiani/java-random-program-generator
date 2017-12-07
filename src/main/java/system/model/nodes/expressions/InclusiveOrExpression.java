a76b6fc5274f0d62020bba6d25c528856bd48179
Riccardo Pressiani <riccardo.pressiani@gmail.com>
2017-12-07 11:48:32 -0600

    Add inclusive or

--- src/main/java/system/model/nodes/expressions/InclusiveOrExpression.java
+++ src/main/java/system/model/nodes/expressions/InclusiveOrExpression.java
@@ -3,23 +3,37 @@ package system.model.nodes.expressions;
 import system.model.STKey;
 import system.model.ScopeTable;
 import system.model.nodes.Node;
+import utils.RandomGen;
 
 //inclusiveOrExpression
 //        :	exclusiveOrExpression
 //        |	inclusiveOrExpression '|' exclusiveOrExpression
 //        ;
 
-//TODO inclusiveOrExpression '|' exclusiveOrExpression
 public class InclusiveOrExpression implements Node {
 
     private ExclusiveOrExpression exclusiveOrExpression;
+    private InclusiveOrExpression inclusiveOrExpression;
+
+    private boolean unary;
 
     InclusiveOrExpression(STKey key, ScopeTable scopeTable) {
+        this.unary = (RandomGen.getNextInt(10) != 0);
+
         this.exclusiveOrExpression = new ExclusiveOrExpression(key, scopeTable);
+
+        if (!this.unary) {
+            this.inclusiveOrExpression = new InclusiveOrExpression(key, scopeTable);
+        }
     }
 
     @Override
     public String produce() {
-        return this.verify(exclusiveOrExpression.produce());
+        if (this.unary) {
+            return this.verify(exclusiveOrExpression.produce());
+        } else {
+            String prod = this.inclusiveOrExpression.produce() + "|" + this.exclusiveOrExpression.produce();
+            return this.verify(prod);
+        }
     }
 }
