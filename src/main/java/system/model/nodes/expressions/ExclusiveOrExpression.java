fe2af1819aa3f77709b6d6a7304ab5ed69105a37
Riccardo Pressiani <riccardo.pressiani@gmail.com>
2017-12-07 16:14:56 -0600

    Add exclusive or

--- src/main/java/system/model/nodes/expressions/ExclusiveOrExpression.java
+++ src/main/java/system/model/nodes/expressions/ExclusiveOrExpression.java
@@ -3,23 +3,37 @@ package system.model.nodes.expressions;
 import system.model.STKey;
 import system.model.ScopeTable;
 import system.model.nodes.Node;
+import utils.RandomGen;
 
 //exclusiveOrExpression
 //        :	andExpression
 //        |	exclusiveOrExpression '^' andExpression
 //        ;
 
-//TODO exclusiveOrExpression '^' andExpression
 public class ExclusiveOrExpression implements Node {
 
     private AndExpression andExpression;
+    private ExclusiveOrExpression exclusiveOrExpression;
+
+    private boolean unary;
 
     ExclusiveOrExpression(STKey key, ScopeTable scopeTable) {
+        this.unary = (RandomGen.getNextInt(10) != 0);
+
         this.andExpression = new AndExpression(key, scopeTable);
+
+        if (!this.unary) {
+            this.exclusiveOrExpression = new ExclusiveOrExpression(key, scopeTable);
+        }
     }
 
     @Override
     public String produce() {
-        return this.verify(andExpression.produce());
+        if (this.unary) {
+            return this.verify(andExpression.produce());
+        } else {
+            String prod = this.exclusiveOrExpression.produce() + "^" + this.andExpression.produce();
+            return this.verify(prod);
+        }
     }
 }
