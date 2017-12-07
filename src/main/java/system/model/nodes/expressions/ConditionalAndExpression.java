1ca0e65ba8e6a4a3490973bb8ec63236ca3808e6
Riccardo Pressiani <riccardo.pressiani@gmail.com>
2017-12-07 10:45:15 -0600

    Add AND

--- src/main/java/system/model/nodes/expressions/ConditionalAndExpression.java
+++ src/main/java/system/model/nodes/expressions/ConditionalAndExpression.java
@@ -3,23 +3,39 @@ package system.model.nodes.expressions;
 import system.model.STKey;
 import system.model.ScopeTable;
 import system.model.nodes.Node;
+import utils.RandomGen;
 
 //conditionalAndExpression
 //        :	inclusiveOrExpression
 //        |	conditionalAndExpression '&&' inclusiveOrExpression
 //        ;
 
-//TODO conditionalAndExpression '&&' inclusiveOrExpression
 public class ConditionalAndExpression implements Node {
 
     private InclusiveOrExpression inclusiveOrExpression;
+    private ConditionalAndExpression conditionalAndExpression;
+
+    private boolean unary;
 
     ConditionalAndExpression(STKey key, ScopeTable scopeTable) {
+        this.unary = (RandomGen.getNextInt(10) != 0);
+
         this.inclusiveOrExpression = new InclusiveOrExpression(key, scopeTable);
+
+        if (!this.unary) {
+            this.conditionalAndExpression = new ConditionalAndExpression(key, scopeTable);
+        }
+
     }
 
     @Override
     public String produce() {
-        return this.verify(inclusiveOrExpression.produce());
+        if (this.unary) {
+            return this.verify(inclusiveOrExpression.produce());
+        } else {
+            String prod = this.conditionalAndExpression.produce() + "&&" + this.inclusiveOrExpression.produce();
+            return this.verify(prod);
+        }
+
     }
 }
