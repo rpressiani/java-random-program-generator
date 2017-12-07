0f7105ab49f1d4a07835715f59b1e85038e49c93
Riccardo Pressiani <riccardo.pressiani@gmail.com>
2017-12-07 16:17:23 -0600

    Add and expression (&)

--- src/main/java/system/model/nodes/expressions/AndExpression.java
+++ src/main/java/system/model/nodes/expressions/AndExpression.java
@@ -3,23 +3,38 @@ package system.model.nodes.expressions;
 import system.model.STKey;
 import system.model.ScopeTable;
 import system.model.nodes.Node;
+import utils.RandomGen;
 
 //andExpression
 //        :	equalityExpression
 //        |	andExpression '&' equalityExpression
 //        ;
 
-//TODO andExpression '&' equalityExpression
 public class AndExpression implements Node {
 
     private EqualityExpression equalityExpression;
+    private AndExpression andExpression;
+
+    private boolean unary;
 
     AndExpression(STKey key, ScopeTable scopeTable) {
+        this.unary = (RandomGen.getNextInt(10) != 0);
+
         this.equalityExpression = new EqualityExpression(key, scopeTable);
+
+        if (!this.unary) {
+            this.andExpression = new AndExpression(key, scopeTable);
+        }
     }
 
     @Override
     public String produce() {
-        return this.verify(equalityExpression.produce());
+        if (this.unary) {
+            return this.verify(equalityExpression.produce());
+        } else {
+            String prod = this.andExpression.produce() + "&" + this.equalityExpression.produce();
+            return this.verify(prod);
+        }
+
     }
 }
