5f73d68edf70f321badb3d3e30895485c2d66f94
Riccardo Pressiani <riccardo.pressiani@gmail.com>
2017-12-07 16:21:25 -0600

    Add equality expression

--- src/main/java/system/model/nodes/expressions/EqualityExpression.java
+++ src/main/java/system/model/nodes/expressions/EqualityExpression.java
@@ -3,6 +3,9 @@ package system.model.nodes.expressions;
 import system.model.STKey;
 import system.model.ScopeTable;
 import system.model.nodes.Node;
+import utils.RandomGen;
+
+import java.util.Random;
 
 //equalityExpression
 //        :	relationalExpression
@@ -10,18 +13,32 @@ import system.model.nodes.Node;
 //        |	equalityExpression '!=' relationalExpression
 //        ;
 
-//TODO equalityExpression '==' relationalExpression
-//TODO equalityExpression '!=' relationalExpression
 public class EqualityExpression implements Node {
 
     private RelationalExpression relationalExpression;
+    private EqualityExpression equalityExpression;
+    private String operator;
+
+    private boolean unary;
 
     EqualityExpression(STKey key, ScopeTable scopeTable) {
+        this.unary = (RandomGen.getNextInt(10) != 0);
+
         this.relationalExpression = new RelationalExpression(key, scopeTable);
+
+        if (!this.unary) {
+            this.equalityExpression = new EqualityExpression(key, scopeTable);
+            this.operator = new Random().nextBoolean() ? "==" : "!=";
+        }
     }
 
     @Override
     public String produce() {
-        return this.verify(relationalExpression.produce());
+        if (this.unary) {
+            return this.verify(relationalExpression.produce());
+        } else {
+            String prod = this.equalityExpression.produce() + this.operator + this.relationalExpression.produce();
+            return this.verify(prod);
+        }
     }
 }
